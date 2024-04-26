9f8d943271693148fbef154daac929ced0d5dfe3
Jabreu0715 <127450942+Jabreu0715@users.noreply.github.com>
2024-04-26 13:03:22 -0400

    Created basic Javascript for form and added typewriter function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: ["Chocolate Cake Recipe coming right up..."],
+    autoStart: true,
+    cursor: null,
+    delay: 75,
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
