1f75366d1fd38eb89c5da8ac0b4d3ceecbaed514
Jabreu0715 <127450942+Jabreu0715@users.noreply.github.com>
2024-05-01 11:59:46 -0400

    Added the AI API integration and revised background image setting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+function displayRecipe(response) {
+  console.log("recipe generated");
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
+    autoStart: true,
+    cursor: null,
+    delay: 25,
+  });
+}
+
 function generateRecipe(event) {
   event.preventDefault();
 
-  new Typewriter("#recipe", {
-    strings: ["Chocolate Cake Recipe coming right up..."],
-    autoStart: true,
-    cursor: null,
-    delay: 75,
-  });
+  let instructionsInput = document.querySelector("#user-text");
+  let apiKey = "23ftbd4aecd5fa6f4304ea2800dofdbf";
+  let prompt = `User instructions: Generate a delicious ${instructionsInput.value} dessert recipe`;
+  let context =
+    "You are an expert in delicious desserts and you love simple dessert recipes. Your mission is to generate a delicious dessert recipe in basic HTML. Please follow the user instructions carefully. Please include estimated cooking time, how many servings per recipe, serving size and calories per serving. ";
+
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating Recipe");
+  console.log(`Prompt:${prompt}`);
+  console.log(`Context:${context}`);
+  axios.get(apiUrl).then(displayRecipe);
 }
 
 let recipeFormElement = document.querySelector("#recipe-generator-form");
